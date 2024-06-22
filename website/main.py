ec1b3a659659aefebdb5640767413c0593c2071a
Soumyadeep-Dey36 <rdrishidey36@gmail.com>
2024-06-22 22:22:48 +0530

    initial commit

---
+++ website/main.py
@@ -0,0 +1,102 @@
+import base64,json
+import logging
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import os
+from ultralytics import YOLO
+import cv2
+from predict_img import *
+from recommend_img import *
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+
+# app = Flask(__name__, static_url_path='static')
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.secret_key = "xshgfdyhjsfyujc"
+
+@app.route('/')
+def home():
+    return render_template("home_page.html")
+
+@app.route("/redirect_second")
+def redirect_second():
+    return render_template("second_page.html")
+
+@app.route('/save_data', methods=['POST'])
+def save_data():
+    try:
+        data = request.json
+        if 'image' not in data:
+            return jsonify(status='error', message='No image data found'), 400
+        
+        dataURL = data['image']
+        app.logger.debug("Received data: %s", dataURL)  # Log the first 30 characters for debugging
+
+        # Extract base64 part of the data URL
+        base64_str = dataURL.split(',')[1]
+        
+        # Decode the Base64 string
+        image_data = base64.b64decode(base64_str)
+        
+        # Save the image to a file
+        img_dir = os.path.join('.', 'website/static/images')
+        # img_dir = 'C:/Users/User/Downloads/HackOn/website/static/images'
+        img_path = os.path.join(img_dir, 'snap.jpg')
+        
+        # cv2.imwrite(img_path, image_data)
+        with open(img_path, "wb") as f:
+            f.write(image_data)
+        
+        # predict function
+        predict()
+        
+        return jsonify(status='success', message='Image uploaded successfully')
+    except Exception as e:
+        app.logger.error("Error: %s", e)  # Log the error
+        return jsonify(status='error', message=str(e)), 400
+    
+    
+@app.route('/img_recommend',  methods=['POST'])
+def img_recommend():
+    try:
+        # Requesting data from html file
+        data = request.form['data']
+        app.logger.debug(f"Received data: {data}")
+        
+        # Path to the JSON file
+        json_file_path = os.path.join('.', 'website/static/detected_obj.json')
+        
+        # Load JSON file from the file
+        with open(json_file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+
+        # Function to get values by key
+        def get_values_by_key(input_key, json_data):
+            if input_key in json_data:
+                return json_data[input_key]
+            else:
+                return None
+        
+        # Fetching the values from JSON file
+        values = get_values_by_key(data, json_data)
+
+        if values:
+            for item in values:
+                for detected_object_class, photo_name in item.items():
+                    recommend(detected_object_class, photo_name)
+                    app.logger.debug(f"Key: {detected_object_class}, Value: {photo_name}")
+        else:
+            app.logger.debug(f"Key '{data}' not found in the json file.")
+        
+        print("10101010chgvcjhbchj")
+        # render_template('cond_page.html')
+        return jsonify(status='Successful')
+        # return render_template("third.html")
+    except Exception as e:
+        app.logger.error(f"Error in img_recommend: {e}")
+        return jsonify(status='error', message=str(e)), 400
+    # return jsonify(status='success', message='Recommendations uploaded successfully')
+        
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ website/predict_img.py
@@ -0,0 +1,95 @@
+import os
+from ultralytics import YOLO
+import cv2, json, sys
+
+def predict():
+    # Define paths
+    # IMAGE_DIR = 'C:/Users/User/Downloads/HackOn/website/images'
+    IMAGE_DIR = os.path.join('.', 'website/static/images')
+    DICTIONARY_DIR = os.path.join('.', 'website/static')
+    image_path = os.path.join(IMAGE_DIR, 'snap.jpg')
+    image_path_out = os.path.join(IMAGE_DIR, 'snap_out.jpg')
+    cropped_dir = os.path.join(IMAGE_DIR, 'yolo_out')
+    json_path = os.path.join(DICTIONARY_DIR, 'detected_obj.json')
+    model_path = os.path.join('.', 'website/yolo_model.pt')
+    
+    def delete_files_in_folder(folder_path):
+        try:
+            # Iterate over all files in the folder
+            for filename in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, filename)
+                
+                # Check if the path is a file and not a directory
+                if os.path.isfile(file_path):
+                    os.remove(file_path)  # Delete the file
+                    print(f"Deleted: {file_path}")
+            
+            print(f"All files deleted from {folder_path}")
+        except Exception as e:
+            print(f"Error deleting files: {e}")
+
+    # Example usage:
+    delete_files_in_folder(cropped_dir)
+
+    # Create directory for cropped images if it doesn't exist
+    os.makedirs(cropped_dir, exist_ok=True)
+
+    # Load a model
+    model = YOLO(model_path)  # load a custom model
+
+    # Read the image
+    image = cv2.imread(image_path)
+    H, W,_  = image.shape
+
+    # Define threshold for filtering detections
+    threshold = 0.5
+
+    # Perform inference
+    results = model(image)[0]
+
+    # Initialize counter for detected objects
+    detected_object_count = 0
+    
+    # Initialize dictionary to store labels and filenames
+    detections_dict = {}
+
+    # Process and annotate results
+    cropped_count = 0
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        if score > threshold:
+            # Increment the detected object counter
+            detected_object_count += 1
+
+            # Get the label
+            label = results.names[int(class_id)].upper()
+
+            # Crop the detected region
+            crop_img = image[int(y1):int(y2), int(x1):int(x2)]
+
+            # Save the cropped image with class label in the filename
+            crop_img_filename = f'{label}crop{cropped_count}.jpg'
+            crop_img_path = os.path.join(cropped_dir, crop_img_filename)
+            cv2.imwrite(crop_img_path, crop_img)
+            cropped_count += 1
+
+            # Append the cropped image path to the detections dictionary
+            if label not in detections_dict:
+                detections_dict[label] = []
+            detections_dict[label].append({label: crop_img_filename})
+
+    # Create a final dictionary with serial numbers
+    final_dict = {str(i): detections for i, (label, detections) in enumerate(detections_dict.items())}
+
+    # Save detections to JSON file
+    with open(json_path, 'w') as json_file:
+        json.dump(final_dict, json_file, indent=4)
+
+    # Print the dictionary
+    print(final_dict)
+
+    # Print the number of objects detected
+    print(f'Number of objects detected: {detected_object_count}')
+    
+# predict()
